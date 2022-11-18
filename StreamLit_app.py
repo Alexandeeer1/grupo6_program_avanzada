3ee0510f3a1e0ec5a6805be4f7ebda56d7248c2a
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-18 09:23:29 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -1,12 +1,30 @@
+###########
+#Librerias#
+###########
 import pandas as pd
 import streamlit as st
 
+########
+#Titulo#
+########
 st.title("SUNEDU - Licenciamiento Institucional")
 st.write("-----------------------------")
+
+#############
+#Integrantes#
+#############
 st.subheader("""Integrantes:
 - Peñaloza Huaman, Bryan Alexander
 - Rodriguez Reategui, Rodrigo Alonso
 - Oviedo Chahua, Gilmar Rony 
 - Castro Pichihua, Victoria Beatriz 
              """)
+st.write("-----------------------------")
+
+###########################
+#Lectura De Datos En Excel#
+###########################
+
+Licencia_Institucional=pd.read_csv("https://www.datosabiertos.gob.pe/sites/default/files/Licenciamiento%20Institucional_7.csv")
+st.write(Licencia_Institucional)
 

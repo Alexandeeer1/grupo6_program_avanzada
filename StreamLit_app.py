379bcf4ed5b59b60f933abb3cf8e8680f3c24258
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-18 08:55:22 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -3,5 +3,10 @@ import streamlit as st
 
 st.title("SUNEDU - Licenciamiento Institucional")
 st.write("-----------------------------")
-
+st.subheader("""Integrantes:
+- Peñaloza Huaman, Bryan Alexander
+- Rodriguez Reategui, Rodrigo Alonso
+- Oviedo Chahua, Gilmar Rony 
+- Castro Pichihua, Victoria Beatriz 
+             """)
 

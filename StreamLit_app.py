efd1acdd6120d3cc8ad609794ea354bab4dfd7c7
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-18 10:27:28 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -34,7 +34,7 @@ st.write("-----------------------------")
 #Lectura De Datos En Excel#
 ###########################
 
-Licencia_Institucional=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento-Institucional_7.xlsx', header= 0)
+Licencia_Institucional=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento-Institucional_7.xlsx')
 st.write(Licencia_Institucional)
 
 Diccionario_Licencia=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento%20Institucional%20-%20Diccionario_1.xlsx', header= 0)

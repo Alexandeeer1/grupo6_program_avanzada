330482e5f779479004637a0556427f3e0cc0361e
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-20 18:23:24 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -60,7 +60,3 @@ Licencia_Institucional=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_pr
 st.write(Licencia_Institucional)
 st.write("-----------------------------")
 
-Diccionario_Licencia=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento%20Institucional%20-%20Diccionario_1.xlsx', header= 0)
-st.write(Diccionario_Licencia)
-
-

cf741dd4e33aca3e05d58525f8037de9d0c45aa5
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-11-20 18:29:16 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -58,8 +58,10 @@ columnas=["CODIGO_ENTIDAD",
 
 Licencia_Institucional=pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento%20Institucional_7.xlsx', header=1, names=columnas)
 
-st.pydeck_chart(LATITUD)
+st.bar_chart(Licencia_Institucional)
+  
 
 st.write(Licencia_Institucional)
+
 st.write("-----------------------------")
 

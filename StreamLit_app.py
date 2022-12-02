30bb19a5d103e848737aad20fe3113f772b62707
Alexandeeer1 <117461904+Alexandeeer1@users.noreply.github.com>
2022-12-02 07:40:27 -0600

    Update StreamLit_app.py

--- StreamLit_app.py
+++ StreamLit_app.py
@@ -67,12 +67,11 @@ columnas=["CODIGO_ENTIDAD",
           "LONGITUD",
           "FECHA_CORTE"]
 
-Licencia_Institucional = pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento%20Institucional_7.xlsx', header=1, names=columnas)  
-st.write(Licencia_Institucional)
-
+Licencia_Institucional = pd.read_excel(r'https://github.com/Alexandeeer1/grupo6_program_avanzada/raw/main/Licenciamiento%20Institucional_7.xlsx', header=1, names=columnas)
+st.write("-----------------------------")
+st.dataframe(Licencia_Institucional)
 st.write("-----------------------------")
 
-st.dataframe(Licencia_Institucional)
 opcion = st.selectbox(
     'Eliga algún departamento del Perú en el botón de abajo',
     ('Email', 'Home phone', 'Mobile phone'))
